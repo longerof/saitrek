b1bf12efdbc82ea1e5db0146654cb3142cdf92f6
isaas-jesus228 <glebvorobev6@gmail.com>
2021-06-06 20:53:00 +0500

    ipip

--- logistik.py
+++ logistik.py
@@ -1,6 +1,3 @@
 import sqlite3
+import flask
 
-with sqlite3.connect('db/database.db') as db:
-    cursor = db.cursor()
-    query = """CREATE TABLE IF NOT EXISTS users(id INTEGER, name TEXT)"""
-    cursor.execute(query)
