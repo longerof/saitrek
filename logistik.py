18b11bb24a16efb8458b8c0393f15db5ec79540e
longerof <iluxa.kul@gmail.com>
2021-06-15 21:18:22 +0500

    ббждб

--- logistik.py
+++ logistik.py
@@ -1,3 +1,13 @@
 import sqlite3
 import flask
+from flask import Flask
+from flask import render_template
 
+
+app = Flask(__name__)
+
+@app.route("/")
+def project():
+    return render_template("project.html")
+
+app.run()
