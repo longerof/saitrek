6e7e24bcc745976e9b1646eae44d705a5430b31e
longerof <iluxa.kul@gmail.com>
2021-06-16 21:10:56 +0500

    dsdasds

--- logistik.py
+++ logistik.py
@@ -6,20 +6,20 @@ from flask import render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/url.main")
 def project():
     return render_template("project.html")
 
-@app.route("/")
+@app.route("/url.login")
 def log():
     return render_template("login.html")
 
-@app.route("/")
+@app.route("/url.register")
 def reg():
     return render_template("reg.html")
 
 @app.route("/addblog")
 def addblog():
-    return render_template("create.html")
+    return render_template("create.html", methods=['POST'])
 
 app.run()
