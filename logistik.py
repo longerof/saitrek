c1aecbe9b7f20df30b962646a063c0074a841546
MikeRoma45 <61829843+MikeRoma45@users.noreply.github.com>
2021-06-17 16:10:50 +0500

    шрау

--- logistik.py
+++ logistik.py
@@ -1,11 +1,37 @@
 import sqlite3
 from flask import Flask
 from flask import render_template
+from flask import request
+from flask import redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template("project.html")
+
+@app.route('/reg')
+def reg():
+    return render_template("reg.html")
+
+@app.route('/reg/fun', methods=['POST'])
+def reg_fun():
+    family = request.form["family"]
+    email = request.form["email"]
+    psw = request.form["psw"]
+    connect = sqlite3.connect("database.db")
+    cursor = connect.cursor()
+
+    cursor.execute("INSERT INTO users (family, email, psw) VALUES (?, ?, ?)", (family, email, psw))
+    connect.commit()
+    return redirect("/")
+
+@app.route('/log')
+def log():
+    return render_template("login.html")
+
+@app.route('/log/fun')
+def log_fun():
+    return ""
 
 app.run()
